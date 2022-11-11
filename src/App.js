b82b8b08f2f81faca507d9745e10e5c4609c76e4
Alynson Roldan <alynsonrolfu@gmai.com>
2022-11-11 14:10:51 -0300

    hicimos un carrito de o

--- src/App.js
+++ src/App.js
@@ -4,27 +4,55 @@ import Todo from './components/Todo';
 
 
 function App() {
-const defaultState =[
-  {label:"comprar mantequilla"},
-  {label:"comprar pan"},
-  {label:"pagar la luz"}
-]
+  const defaultState = [
+    { label: "comprar mantequilla" },
+    { label: "comprar pan" },
+    { label: "pagar la luz" }
+  ]
 
-const [items, setItems] = useState(defaultState)
+  const [items, setItems] = useState(defaultState)
+  const [newItem, setNewItem] = useState('')
 
-const list = items.map(
-  item => {
-return <Todo>{item.label}</Todo>
+
+
+  const onClickFn = () => {
+    setItems([...items, { label: newItem }])
   }
-)
+
+  const onChangeFn = (event) => {
+    setNewItem(event.target.value)
+  }
+  const deleteFn = (label) => {
+    setItems(items.filter(item => item.label !== label))
+  }
+const deleteNm = () => {
+  setItems ([])
+}
+
+  const list = items.map(
+    item => {
+      return <Todo click={() => deleteFn(item.label)} >{item.label}</Todo>
+    }
+  )
+
   return (
     <div className="App">
       <h1>Mi lista de cosas por hacer</h1>
 
-      <input type ="text"/>
-      <button>Agregar</button>
+      <input
+        type="text"
+        value={newItem}
+        onChange={onChangeFn}
+      />
+      <button onClick={onClickFn}>Agregar</button>
+
 
       {list}
+      <h2>total de cosas en la lista: {items.length}</h2>
+
+      <br />
+      <button onClick={deleteNm}>eliminar Todo 🗑️</button>
+
     </div>
   );
 }
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,8 +1,12 @@
 const Todo = (props) => {
     return (
         <div>
-            <input type = "checkbox" />
+            <input type="checkbox" />
             <label>{props.children}</label>
+            <span>
+                <button onClick = {props.click}>🗑️</button>
+            </span>
+
         </div>
     )
 }
